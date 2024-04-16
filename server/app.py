97b893d4d9f6e3446720a35dd78f497bdcb8d6cb
+collins <+collinskipngetich60@gmail.com>
2024-04-16 13:37:45 +0300

    done

--- server/app.py
+++ server/app.py
@@ -7,20 +7,93 @@ from sqlalchemy.exc import IntegrityError
 from config import app, db, api
 from models import User, Recipe
 
+from flask import jsonify
+from flask_restful import reqparse
+from sqlalchemy.exc import IntegrityError
+from models import User, Recipe
+
+
 class Signup(Resource):
-    pass
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('username', type=str, required=True)
+        parser.add_argument('password', type=str, required=True)
+        parser.add_argument('image_url', type=str)
+        parser.add_argument('bio', type=str)
+        args = parser.parse_args()
+
+        username = args['username']
+        password = args['password']
+        image_url = args.get('image_url')
+        bio = args.get('bio')
+
+        new_user = User(username=username, password=password, image_url=image_url, bio=bio)
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+            return jsonify({'message': 'User created successfully', 'user_id': new_user.id}), 201
+        except IntegrityError:
+            db.session.rollback()
+            return jsonify({'error': 'Username already exists'}), 422
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        if 'user_id' in session:
+            user_id = session['user_id']
+            user = User.query.get(user_id)
+            return jsonify({'user_id': user.id, 'username': user.username, 'image_url': user.image_url, 'bio': user.bio}), 200
+        else:
+            return jsonify({'error': 'Unauthorized'}), 401
 
 class Login(Resource):
-    pass
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('username', type=str, required=True)
+        parser.add_argument('password', type=str, required=True)
+        args = parser.parse_args()
+
+        username = args['username']
+        password = args['password']
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.check_password(password):
+            session['user_id'] = user.id
+            return jsonify({'user_id': user.id, 'username': user.username, 'image_url': user.image_url, 'bio': user.bio}), 200
+        else:
+            return jsonify({'error': 'Invalid username or password'}), 401
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        if 'user_id' in session:
+            session.pop('user_id')
+            return '', 204
+        else:
+            return jsonify({'error': 'Unauthorized'}), 401
 
 class RecipeIndex(Resource):
-    pass
+    def get(self):
+        if 'user_id' in session:
+            recipes = Recipe.query.all()
+            serialized_recipes = []
+            for recipe in recipes:
+                serialized_recipe = {
+                    'title': recipe.title,
+                    'instructions': recipe.instructions,
+                    'minutes_to_complete': recipe.minutes_to_complete,
+                    'author': {
+                        'user_id': recipe.author.id,
+                        'username': recipe.author.username,
+                        'image_url': recipe.author.image_url,
+                        'bio': recipe.author.bio
+                    }
+                }
+                serialized_recipes.append(serialized_recipe)
+            return jsonify(serialized_recipes), 200
+        else:
+            return jsonify({'error': 'Unauthorized'}), 401
+
+
 
 api.add_resource(Signup, '/signup', endpoint='signup')
 api.add_resource(CheckSession, '/check_session', endpoint='check_session')
